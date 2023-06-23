0e0aa3d658a6df621e5aefbc2e2dbd959e85a9f5
DhitGT <adityadwisaputra666@gmail.com>
2023-06-23 22:11:34 +0700

    2

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
               }
             />
             <Route
-              path="/"
+              path="/anime"
               element={
                 <HomePage
                   animeData={animeTop}
